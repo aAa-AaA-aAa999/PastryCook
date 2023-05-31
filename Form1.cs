34224456ec4a8e8af7922896b80033c5d3e64c44
7x7x49.mrr <98632330+kitty7x7x49@users.noreply.github.com>
2023-05-31 12:41:53 +0300

    Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -306,6 +306,49 @@ namespace PastryCook
         }
     }
     //
+    // логирование
+    //
+    public class LogWriter
+    {
+        private string m_exePath = string.Empty;
+        public LogWriter(string logMessage)
+        {
+            LogWrite(logMessage);
+        }
+        public void LogWrite(string logMessage)
+        {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter w = File.AppendText(@"D:\сишарп\PastryCook\log.txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                Console.Write("\r\nЛог : ");
+                txtWriter.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+                txtWriter.WriteLine("  :");
+                txtWriter.WriteLine("  :{0}", logMessage);
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    
+    //
     //отладочный класс
     //
     public class Debugger
